86c4364a3433bbee47a372f916d74e7a491d01c5
Jason Barto <jason.p.barto@gmail.com>
2017-03-14 23:20:25 +0000

    updated snippets to the latest CF specification

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+curl -o CloudFormationResourceSpecification.json https://d3teyb21fexa9r.cloudfront.net/latest/CloudFormationResourceSpecification.json
 python generate-snippets.py
 rm -fr snippets/*
 mv snippets.generated/* snippets
